a6ce628249b61288fa42db4147391c5d71733b57
Fauzy169 <ahmadfauzy169@gmail.com>
2024-12-12 14:24:04 +0800

    Uppdate Features

---
+++ app/Http/Controllers/TodoController.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Todo;
+use Illuminate\Http\Request;
+
+class TodoController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $data = Todo::orderBy('task','asc')->get();
+        return view('todo.app', ['data' => $data]);
+
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'task' => 'required|min:3|max:30'
+        ],[
+            'task.required' => 'Perlu Untuk Diisikan',
+            'task.min' => 'minimal mengisi 3 Karakter',
+            'task.max' => 'maksimal karakter yang diisikan itu 30 Karakter'
+        ]);
+
+        $data = [
+            'task' => $request->input('task')
+        ];
+
+        Todo::create($data);
+        return redirect()->route('todo')->with('success', 'Task Baru Berhasil Di Tambahkan');
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        $request->validate([
+            'task' => 'required|min:3|max:30'
+        ],[
+            'task.required' => 'Perlu Untuk Diisikan',
+            'task.min' => 'minimal mengisi 3 Karakter',
+            'task.max' => 'maksimal karakter yang diisikan itu 30 Karakter'
+        ]);
+
+        $data = [
+            'task' => $request->input('task'),
+            'is_done' => $request->input('is_done')
+        ];
+
+        Todo::where('id',$id)->update($data);
+        return redirect()->route('todo')->with('success', 'Task Berhasil Di Update');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
+use App\Http\Controllers\TodoController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get('/todo', [TodoController::class, 'index'])->name('todo');
+Route::post('/todo', [TodoController::class, 'store'])->name('todo.post');
+Route::put('/todo/{id}', [TodoController::class, 'update']);
